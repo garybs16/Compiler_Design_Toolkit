887834b6a8e83e512406f43457c980e776b424d8
anarotgo <anarotgo@yahoo.com>
2025-03-13 12:48:18 -0700

    Added lexer.rb and source_code.txt

---
+++ lexer.rb
@@ -0,0 +1,50 @@
+# Lexical analyzer
+# Langauge: Ruby
+# Developed by: Anar Otgonbaatar, Gary, Dan
+
+class Lexer
+	# Define token categories using regex and lists
+	KEYWORDS = 
+	OPERATORS = 
+	SEPARATORS = 
+
+	def intialize( file_path )
+		@file_path = file_path
+		@tokens = []	# Store final tokens as [lexeme, token_type]
+	end
+
+	# Read file and lexically analyze
+	def analyze
+		content = File.read( @file_path )
+		content = remove_comments( content )
+		tokenize( content )
+		display_tokens
+	end
+
+	# Remove comments
+	def remove_comments( code )
+		code.gsub!()	# Single line comments
+		code.gsub!()	# Multi line comments
+		code
+	end
+
+	# Tokenize
+	def tokenize( code )
+		# Regex patterns to id lexemes
+		pattern = /
+		/x
+
+		code.scan( pattern ).each do |lexeme|
+			classify_token( lexeme.strip ) unless lexeme.strip.empty?
+		end
+	end
+
+	# Classify lexemes into token categories
+	def classify_token( lexeme )
+
+	end
+
+	# Display tokens
+	def display_tokens
+
+	end
